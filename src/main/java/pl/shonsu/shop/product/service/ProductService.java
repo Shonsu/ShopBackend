78c9124e8ce9ad16a72869aed4f920fed3dafbce
Shonsu <shonsu82@gmail.com>
2022-11-13 00:03:58 +0100

    adding product pagination

--- src/main/java/pl/shonsu/shop/product/service/ProductService.java
+++ src/main/java/pl/shonsu/shop/product/service/ProductService.java
@@ -1,18 +1,18 @@
 package pl.shonsu.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.shonsu.shop.product.model.Product;
 import pl.shonsu.shop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
