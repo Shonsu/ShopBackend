6b3400b64b99a00155ff63a7c7079070bbb22f16
Shonsu <shonsu82@gmail.com>
2022-12-09 18:08:06 +0100

    fixied n+1 problem with mapping dto

--- src/main/java/pl/shonsu/shop/category/model/CategoryProductsDto.java
+++ src/main/java/pl/shonsu/shop/category/model/CategoryProductsDto.java
@@ -1,7 +1,7 @@
 package pl.shonsu.shop.category.model;
 
 import org.springframework.data.domain.Page;
-import pl.shonsu.shop.product.model.Product;
+import pl.shonsu.shop.product.controller.dto.ProductListDto;
 
-public record CategoryProductsDto(Category category, Page<Product> products) {
+public record CategoryProductsDto(Category category, Page<ProductListDto> products) {
 }
