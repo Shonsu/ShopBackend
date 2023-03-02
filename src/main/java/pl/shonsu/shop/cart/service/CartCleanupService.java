1d81e3103283308ab67863dfd9e6d035549c21d5
Shonsu <shonsu82@gmail.com>
2023-03-02 11:51:47 +0100

    fix logging

--- src/main/java/pl/shonsu/shop/cart/service/CartCleanupService.java
+++ src/main/java/pl/shonsu/shop/cart/service/CartCleanupService.java
@@ -1,7 +1,6 @@
 package pl.shonsu.shop.cart.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +12,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class CartCleanupService {
 
--- src/main/java/pl/shonsu/shop/passwordresettoken/service/PasswordResetTokenCleanupService.java
+++ src/main/java/pl/shonsu/shop/passwordresettoken/service/PasswordResetTokenCleanupService.java
@@ -1,6 +1,7 @@
 package pl.shonsu.shop.passwordresettoken.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,16 +13,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class PasswordResetTokenCleanupService {
     private final PasswordResetTokenRepository passwordResetTokenRepository;
 
     @Transactional
-    @Scheduled(cron = "${app.passwordResetToken.cleanup.expression}" )
+    @Scheduled(cron = "${app.passwordResetToken.cleanup.expression}")
     void cleanupExpiredTokens() {
-        List<PasswordResetToken> prt =  passwordResetTokenRepository.findByTokenExpiryLessThan(LocalDateTime.now());
+        List<PasswordResetToken> prt = passwordResetTokenRepository.findByTokenExpiryLessThan(LocalDateTime.now());
         List<PasswordResetTokenId> ids = prt.stream().map(PasswordResetToken::getId).toList();
-        prt.forEach(passwordResetTokenId -> System.out.println("token: " + passwordResetTokenId.getId().getToken() + " expiry: " + passwordResetTokenId.getTokenExpiry()));
-        passwordResetTokenRepository.deleteAllByIdIn(ids);
+        if (!ids.isEmpty()) {
+            prt.forEach(passwordResetTokenId -> log.info("token: " + passwordResetTokenId.getId().getToken() + " expiry: " + passwordResetTokenId.getTokenExpiry()));
+            passwordResetTokenRepository.deleteAllByIdIn(ids);
+        }
     }
 }
