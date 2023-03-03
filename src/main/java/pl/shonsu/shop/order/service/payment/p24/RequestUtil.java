b1dc84454d8a4f1ea5c803652d526db7ce80e9aa
Shonsu <shonsu82@gmail.com>
2023-03-03 23:27:12 +0100

    problem with adress validation in payment

--- src/main/java/pl/shonsu/shop/order/service/payment/p24/RequestUtil.java
+++ src/main/java/pl/shonsu/shop/order/service/payment/p24/RequestUtil.java
@@ -39,6 +39,7 @@ public class RequestUtil {
     }
 
     public static void validateIpAddress(String remoteAddr, PaymentMethodP24Config config) {
+        System.out.println(remoteAddr);
         if (!config.getServers().contains(remoteAddr)) {
             throw new RuntimeException("Niepoprawny adres IP dla potwierdzenia płatności.");
         }
