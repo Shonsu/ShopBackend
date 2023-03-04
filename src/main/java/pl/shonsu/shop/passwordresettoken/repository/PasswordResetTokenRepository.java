c8cb385de398781495555ae9e57ec5a629d1b6cc
Shonsu <shonsu82@gmail.com>
2023-03-04 12:27:27 +0100

    delete jpql query, add test for tokenreset cleanup service

--- src/main/java/pl/shonsu/shop/passwordresettoken/repository/PasswordResetTokenRepository.java
+++ src/main/java/pl/shonsu/shop/passwordresettoken/repository/PasswordResetTokenRepository.java
@@ -1,7 +1,6 @@
 package pl.shonsu.shop.passwordresettoken.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import pl.shonsu.shop.passwordresettoken.model.PasswordResetToken;
 import pl.shonsu.shop.passwordresettoken.model.PasswordResetTokenId;
@@ -18,7 +17,7 @@ public interface PasswordResetTokenRepository extends JpaRepository<PasswordRese
     @Query("select id from PasswordResetToken")
     List<PasswordResetTokenId> findAllId();
 
-    @Modifying
-    @Query("delete from PasswordResetToken where id in (:ids)")
+//    @Modifying
+//    @Query("delete from PasswordResetToken where id in (:ids)")
     void deleteAllByIdIn(List<PasswordResetTokenId> ids);
 }
---
+++ src/test/java/pl/shonsu/shop/passwordresettoken/service/PasswordResetTokenCleanupServiceTest.java
@@ -0,0 +1,62 @@
+package pl.shonsu.shop.passwordresettoken.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import pl.shonsu.shop.passwordresettoken.model.PasswordResetToken;
+import pl.shonsu.shop.passwordresettoken.model.PasswordResetTokenId;
+import pl.shonsu.shop.passwordresettoken.repository.PasswordResetTokenRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static pl.shonsu.shop.passwordresettoken.utils.TokenUtils.generateToken;
+import static pl.shonsu.shop.passwordresettoken.utils.TokenUtils.hashToken;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class PasswordResetTokenCleanupServiceTest {
+
+    @Autowired
+    PasswordResetTokenCleanupService cleanupService;
+
+    @Autowired
+    PasswordResetTokenRepository tokenRepository;
+
+    @Test
+    @Transactional
+    void shouldCleanupExpiredTokens() {
+        //given
+        createPasswordResetTokens();
+        //when
+        cleanupService.cleanupExpiredTokens();
+        List<PasswordResetToken> resetTokens = tokenRepository.findAll();
+        //then
+        assertEquals(1, resetTokens.size());
+    }
+
+    private void createPasswordResetTokens() {
+
+        List<PasswordResetToken> passwordResetTokens = new ArrayList<>();
+        String token = generateToken();
+        String hashedToken = hashToken(token);
+
+        passwordResetTokens.add(new PasswordResetToken(new PasswordResetTokenId(1L, hashedToken), LocalDateTime.now().minusDays(1), false));
+        token = generateToken();
+        hashedToken = hashToken(token);
+        passwordResetTokens.add(new PasswordResetToken(new PasswordResetTokenId(2L, hashedToken), LocalDateTime.now().minusMinutes(10), false));
+        token = generateToken();
+        hashedToken = hashToken(token);
+        passwordResetTokens.add(new PasswordResetToken(new PasswordResetTokenId(3L, hashedToken), LocalDateTime.now().minusHours(5), false));
+        token = generateToken();
+        hashedToken = hashToken(token);
+        passwordResetTokens.add(new PasswordResetToken(new PasswordResetTokenId(4L, hashedToken), LocalDateTime.now(), false));
+
+        tokenRepository.saveAll(passwordResetTokens);
+    }
+}
